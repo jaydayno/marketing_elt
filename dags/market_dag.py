59b16b6695512a0dc2de9dfc5ac4a3f75d261c3d
jaydayno <jaydendayno@gmail.com>
2023-08-19 12:24:01 -0400

    add fivetran operator and sensor

--- dags/market_dag.py
+++ dags/market_dag.py
@@ -1,4 +1,5 @@
 from fivetran_provider_async.operators import FivetranOperator
+from fivetran_provider_async.sensors import FivetranSensor
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 from airflow import DAG
@@ -17,8 +18,22 @@ with DAG(
     
     ## Task 0 - Creating Connections in Airflow
     initializing_conns = BashOperator(
-        task_id = 'connection_task',
-        bash_command= 'python3 ${AIRFLOW_HOME}/dags/scripts/add_connections.py'
+        task_id='connection_task',
+        bash_command='python3 ${AIRFLOW_HOME}/dags/scripts/add_connections.py'
     )
 
-initializing_conns
+    ## Task 1 - Run Fivetran Connector
+    start_Fivetran_connector = FivetranOperator(
+        task_id='trigger_fivetran',
+        connector_id='gone_formalities', # from Fivetran
+        fivetran_conn_id='my_fivetran' # from Task 0 i.e. Airflow Connection
+    )
+
+    ## Task 2 - Check if gone_formalities ran successfully
+    check_Fivetran_connector = FivetranSensor(
+        task_id='sense_fivetran',
+        connector_id='gone_formalities',
+        fivetran_conn_id='my_fivetran'
+    )
+
+initializing_conns >> [start_Fivetran_connector, check_Fivetran_connector]
